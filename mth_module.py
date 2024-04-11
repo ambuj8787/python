3c12a3a2286ae54fd02f25f920ed6710fae0d5fa
ambuj8787 <165764957+ambuj8787@users.noreply.github.com>
2024-04-11 12:34:49 +0530

    Add files via upload

---
+++ mth_module.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 11:08:53 2023
+
+@author: user
+"""
+import math
+result=math.factorial(10)
+print(result)
+
+print(math.pow(2,3))
+
+print(math.sqrt(16))
+
+import
